b431c2c3684b762f568bd297d3197cbdc348a9b5
Viktor Lukashov <viktor@vaadin.com>
2021-07-05 23:51:22 +0300

    chore: move CI builds from travis-ci.org to GitHub actions
    The CI builds cover the following use cases:
     - run a full set of automated code checks for each PR (see `unit-tests.yml` and `visual-tests.yml` in `.github/workflows/`)
       - when a new PR is open (for any branch having these workflow .yml files)
       - when an open PR gets a new commit (including a force push)
     - do not run automated checks if the commit title includes `skip ci`
       The automated checks are skipped only for that one commit which has `skip ci`. For consequent commits that do not have `skip ci` the checks are executed normally.
    
    The full set of automated tests includes both unit and visual tests:
     - Unit tests are executed with the Polymer 2 source code, and also with the Polymer 3 source code after running the P2-to-P3 conversion script.
       - The unit tests with the Polymer 3 source code are executed in a set of different browsers / platforms (via SauceLabs).
       - The unit tests with the Polymer 2 source code are executed in a few browsers running headlessly in the CI agent.
     - Visual tests are executed with the Polymer 2 source code in Chrome and Firefox (via SauceLabs).
    
    Related to: https://github.com/vaadin/components-team-tasks/issues/586

---
+++ wct.conf.js
@@ -0,0 +1,67 @@
+var envIndex = process.argv.indexOf('--env') + 1;
+var env = envIndex ? process.argv[envIndex] : undefined;
+
+// workaround for Android 7+ blocking all HTTP traffic
+// see https://wiki.saucelabs.com/display/DOCS/Known+Issues
+// add it to your own local `/etc/hosts` to run SauceLabs tests locally
+var tunneledLocalhost = 'localhost-for-saucelabs';
+
+module.exports = {
+  testTimeout: 180 * 1000,
+  verbose: false,
+  plugins: {
+    local: {
+      browserOptions: {
+        chrome: [
+          'headless',
+          'disable-gpu',
+          'no-sandbox'
+        ]
+      }
+    },
+    // MAGI REMOVE START
+    istanbul: {
+      dir: './coverage',
+      reporters: ['text-summary', 'lcov'],
+      include: [
+        '**/vaadin-app-layout/src/*.html'
+      ],
+      exclude: [],
+      thresholds: {
+        global: {
+          statements: 80
+        }
+      }
+    }
+    // MAGI REMOVE END
+  },
+
+  registerHooks: function(context) {
+    const testBrowsers = [
+      {
+        deviceName: 'Android GoogleAPI Emulator',
+        platformName: 'Android',
+        platformVersion: '11.0',
+        browserName: 'Chrome',
+      },
+      'iOS Simulator/iphone@10.3', // should be 9.x, but SauceLabs does not provide that
+      'macOS 11/safari@latest',
+      'Windows 10/microsoftedge@latest',
+      'Windows 10/microsoftedge@18',
+      'Windows 10/internet explorer@11',
+      'Windows 10/chrome@latest',
+      'Windows 10/firefox@latest',
+      'Windows 10/firefox@78', // latest ESR as of 2021-06-30
+    ];
+
+    if (env === 'saucelabs') {
+      context.options.webserver = context.options.webserver || {};
+      context.options.webserver.hostname = tunneledLocalhost;
+      context.options.plugins.sauce.tunnelOptions = {
+        tunnelDomains: tunneledLocalhost
+      };
+
+      context.options.plugins.sauce.browsers = testBrowsers;
+    }
+  }
+};
